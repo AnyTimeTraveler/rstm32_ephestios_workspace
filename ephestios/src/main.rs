5cab98a5a40b68335e0ce3201e5c3dc7ba1c09a5
Simon Struck <simon@simonscode.org>
2023-05-18 20:31:01 +0200

    It builds!

--- ephestios/src/main.rs
+++ ephestios/src/main.rs
@@ -4,13 +4,14 @@
 
 use defmt::unwrap;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output};
+use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::{Duration, Timer};
 
-use {defmt as _, panic_probe as _}; // global logger
+use {defmt_rtt as _, panic_probe as _};
+use embassy_stm32::peripherals::PA0;
 
 #[embassy_executor::task]
-async fn blinker(mut led: Output<'static, P0_13>, interval: Duration) {
+async fn blinker(mut led: Output<'static, PA0>, interval: Duration) {
     loop {
         led.set_high();
         Timer::after(interval).await;
@@ -23,6 +24,6 @@ async fn blinker(mut led: Output<'static, P0_13>, interval: Duration) {
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
-    let led = Output::new(p.P0_13, Level::Low, OutputDrive::Standard);
+    let led = Output::new(p.PA0, Level::Low, Speed::Low);
     unwrap!(spawner.spawn(blinker(led, Duration::from_millis(300))));
 }
